06cc17f57a9261ef43540cdaf53550a64dfbd789
Aydin Qulubeyli <aydin.qulubeyli@gmail.com>
2020-02-04 13:18:56 +0000

    cgi version

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import datetime
 
 def do_magic():
     now = datetime.datetime.now()
-    return "Hello! {0}".format(now)
+    return "Hello Qehbe Etibar! {0}".format(now)
 
 if __name__ == "__main__":
+    print("Content-type: text/html\n\n")
     print(do_magic())
 
